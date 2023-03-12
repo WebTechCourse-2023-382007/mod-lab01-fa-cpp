b1bffab362b4dbabde33715c49b14d1eb1068da2
DmitryRut <126830185+DmitryRut@users.noreply.github.com>
2023-03-12 22:10:49 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -35,9 +35,10 @@ unsigned int faStr2(const char *str) {
           if (isWord) count++;
           isWord = false;
           } else {
-               if (std::isalpha(str[i])) {
-                  if (space && isupper(str[i]) && !isdigit(str[i])) isWord = true;
-                  else if (isWord && isupper(str[i]) || isWord && isdigit(str[i])) isWord = false;
+             if (std::isalpha(str[i])) {
+            if (space && isupper(str[i]) && !isdigit(str[i])) isWord = true;
+            else if (isWord && isupper(str[i]) || isWord && isdigit(str[i]))
+                      isWord = false;
                   space = false;
                   } else { isWord = false; }
                  }
@@ -46,19 +47,19 @@ unsigned int faStr2(const char *str) {
        return count; }
 
 unsigned int faStr3(const char *str) {
-    int countWord = 0, count=0;
+    int countWord = 0, count = 0;
     bool isWord = false, space = true;
     for (int i = 0; i < strlen(str); i++) {
        if (isspace(str[i])) {
        space = true;
        if (isWord) countWord++;
        isWord = false;
-       } else { 
+       } else {
           count++;
           isWord = true;
          }
          }
        if (isWord) countWord++;
        //return (count + countWord / 2) / countWord;
-       return round(<float>(count / countWord));
+       return round((<float>)(count / countWord));
 }
