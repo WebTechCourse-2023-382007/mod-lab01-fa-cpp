a153b2a1a14563ae427ef7fb0debfe20f814442d
DmitryRut <126830185+DmitryRut@users.noreply.github.com>
2023-03-12 21:50:47 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-#include <ctype>
+#include <ctype.h>
 #include <cstring>
 #include <iostream>
 #include <cmath>
 
 int main() {
-	char str[] = { " Hty gre3 ueq N   " };
-	int n1, n2, n3;
-	n1 = faStr1(str);
-	n2 = faStr2(str);
-	n3 = faStr3(str);
-	printf("%d\n", n1);
-	printf("%d\n", n2);
-	printf("%d", n3);
+  char str[] = { " Hty gre3 ueq N   " };
+  int n1, n2, n3;
+  n1 = faStr1(str);
+  n2 = faStr2(str);
+  n3 = faStr3(str);
+  printf("%d\n", n1);
+  printf("%d\n", n2);
+  printf("%d", n3);
 }
