97addf6fbfc744f1275f2381ab788ab0a3bce498
DmitryRut <126830185+DmitryRut@users.noreply.github.com>
2023-03-12 19:34:38 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,12 @@
 #include "fun.h"
 
 int main() {
-    return 0;
+    char str[] = { " Hty gre3 ueq N   " };
+	int n1, n2, n3;
+	n1 = faStr1(str);
+	n2 = faStr2(str);
+	n3 = faStr3(str);
+	printf("%d\n", n1);
+	printf("%d\n", n2);
+	printf("%d", n3);
 }
