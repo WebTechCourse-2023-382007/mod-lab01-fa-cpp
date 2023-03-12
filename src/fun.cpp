f09a8227853c631953e4fb4cb685657e4b741d0e
DmitryRut <126830185+DmitryRut@users.noreply.github.com>
2023-03-12 21:49:31 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -9,58 +9,56 @@ unsigned int faStr1(const char *str) {
     int count = 0;
     bool isWord = false, space = true;
     for (int i = 0; i < strlen(str); i++) {
-		if (isspace(str[i])) {
-			space = true;
-			if (isWord) count++;
-			isWord = false;
-		} else {
-                        if ((space && !isdigit(str[i])) || (isWord && !isdigit(str[i])))
-				isWord = true;
-			if (isdigit(str[i])) {
-				isWord = false;
-				space = false;
-			}
-		}
+       if (isspace(str[i])) {
+       space = true;
+       if (isWord) count++;
+       isWord = false;
+       } else {
+          if ((space && !isdigit(str[i])) || (isWord && !isdigit(str[i])))
+          isWord = true;
+          if (isdigit(str[i])) {
+          isWord = false;
+          space = false;
+        }
+         }
     }
-	if (isWord) count++;
-	return count;
+      if (isWord) count++;
+      return count;
 }
 
 unsigned int faStr2(const char *str) {
-	int count = 0;
-	bool isWord = false, space = true;
-	for (int i = 0; i < strlen(str); i++) {
-		if (isspace(str[i])) {
-			space = true;
-			if (isWord) count++;
-			isWord = false;
-		} else {
-			if (std::isalpha(str[i])) {
-				if (space && isupper(str[i]) && !isdigit(str[i])) isWord = true;
-				else
-					if (isWord && isupper(str[i]) || isWord && isdigit(str[i])) isWord = false;
-				space = false;
-			} else isWord = false;
-		}
-	}
-	if (isWord) count++;
-	return count;
-}
+     int count = 0;
+     bool isWord = false, space = true;
+     for (int i = 0; i < strlen(str); i++) {
+       if (isspace(str[i])) {
+          space = true;
+          if (isWord) count++;
+          isWord = false;
+          } else {
+               if (std::isalpha(str[i])) {
+                  if (space && isupper(str[i]) && !isdigit(str[i])) isWord = true;
+                  else if (isWord && isupper(str[i]) || isWord && isdigit(str[i])) isWord = false;
+                  space = false;
+                  } else { isWord = false; }
+                 }
+              }
+       if (isWord) count++;
+       return count; }
 
 unsigned int faStr3(const char *str) {
-	int countWord = 0, count=0;
-	bool isWord = false, space = true;
-	for (int i = 0; i < strlen(str); i++) {
-		if (isspace(str[i])) {
-			space = true;
-			if (isWord) countWord++;
-			isWord = false;
-		} else { 
-			count++;
-			isWord = true;
-		}
-	}
-	if (isWord) countWord++;
-	//return (count + countWord / 2) / countWord;
-	return round((float)(count / countWord));
+    int countWord = 0, count=0;
+    bool isWord = false, space = true;
+    for (int i = 0; i < strlen(str); i++) {
+       if (isspace(str[i])) {
+       space = true;
+       if (isWord) countWord++;
+       isWord = false;
+       } else { 
+          count++;
+          isWord = true;
+         }
+         }
+       if (isWord) countWord++;
+       //return (count + countWord / 2) / countWord;
+       return round(<float>(count / countWord));
 }
