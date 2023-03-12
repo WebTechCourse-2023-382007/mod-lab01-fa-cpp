e0666a51d0bedcba623bcc3c8006c51ded05e5b1
DmitryRut <126830185+DmitryRut@users.noreply.github.com>
2023-03-12 19:32:49 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,82 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <ctype.h>
+#include <iostream>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int count = 0;
+	bool isWord = false, space = true;
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (isspace(str[i]))
+		{
+			space = true;
+			if (isWord) count++;
+			isWord = false;
+		}
+		else
+		{
+			if (std::isalpha(str[i]))
+			{
+				if ((space && !isdigit(str[i])) || (isWord && !isdigit(str[i])))
+					isWord = true;
+				if (isdigit(str[i]))
+				{
+					isWord = false;
+					space = false;
+				}
+			}
+		}
+	}
+	if (isWord) count++;
+	return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    int count = 0;
+	bool isWord = false, space = true;
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (isspace(str[i]))
+		{
+			space = true;
+			if (isWord) count++;
+			isWord = false;
+		}
+		else
+		{
+			if (std::isalpha(str[i]))
+			{
+				if (space && isupper(str[i]) && !isdigit(str[i])) isWord = true;
+				else
+					if (isWord && isupper(str[i]) || isWord && isdigit(str[i])) isWord = false;
+				space = false;
+			}
+			else isWord = false;
+		}
+	}
+	if (isWord) count++;
+	return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    int countWord = 0, count=0;
+	bool isWord = false, space = true;
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (isspace(str[i]))
+		{
+			space = true;
+			if (isWord) countWord++;
+			isWord = false;
+		}
+		else
+		{
+			count++;
+			isWord = true;
+		}
+	}
+	if (isWord) countWord++;
+	//return (count + countWord / 2) / countWord;
+	return round((float)(count / countWord));
 }
